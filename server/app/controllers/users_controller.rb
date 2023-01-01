edf1cbbfed644a9d3da7105a06a752c265a5a241
Cydiater <cydiater@gmail.com>
2023-01-02 03:35:42 +0800

    upd

---
+++ server/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :set_user, only: [:show]
+
+  def show
+    render json: @user, only: %i[name is_student]
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      head :ok
+    else
+      render json: {
+        msg: @user.errors.full_messages.to_sentence
+      }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :is_student)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
---
+++ server/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: {
+    minimum: 3,
+    maximum: 20
+  }
+  validates :password, presence: true, length: {
+    minimum: 8,
+    maximum: 40
+  }
+  validates :is_student, inclusion: { in: [true, false] }
+end
